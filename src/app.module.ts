6151616a504681692b59a35835dba9936d745911
kakasoo <kscodebase@gmail.com>
2021-07-08 16:59:00 +0900

    chore : 기본적인 리소스 구조 생성 #0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DiaryModule } from './diary/diary.module';
+import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, DiaryModule, GroupModule],
   controllers: [AppController],
   providers: [AppService],
 })
