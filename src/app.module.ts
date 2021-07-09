b2cba876839eafa923430de3f5bf6edfc2c10bf4
kakasoo <kscodebase@gmail.com>
2021-07-09 17:10:03 +0900

    nestjs 기반 typeorm을 이용하여 DB 연동 #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,37 @@ import { UserModule } from './user/user.module';
 import { DiaryModule } from './diary/diary.module';
 import { GroupModule } from './group/group.module';
 import { CommentModule } from './comment/comment.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { Users } from './user/entities/user.entity';
+import { Comments } from './comment/entities/comment.entity';
+import { Dairies } from './diary/entities/diary.entity';
+import { Groups } from './group/entities/group.entity';
 
 @Module({
-  imports: [UserModule, DiaryModule, GroupModule, CommentModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    DiaryModule,
+    GroupModule,
+    CommentModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [Users, Comments, Dairies, Groups],
+      synchronize: false, // NOTE : 개발 환경에서만 사용합니다.
+      charset: 'utf8mb4',
+      logging: true, // NOTE : 개발 시에는 logging을 켜놓는 것이 좋다. ( hot-reloading 으로 인한 지연 방지 )
+      keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
