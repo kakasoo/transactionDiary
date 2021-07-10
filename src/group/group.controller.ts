8521ce66b6f894d3ecc16397e713e2c02f1842f8
kakasoo <kscodebase@gmail.com>
2021-07-10 15:52:01 +0900

    chore : swagger 문서에서 Groups 명세 채우기 #1

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -10,7 +10,7 @@ import {
 import { GroupService } from './group.service';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiParam, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('GROUPS')
 @Controller('api/groups')
@@ -25,6 +25,9 @@ export class GroupController {
     return this.groupService.create(createGroupDto);
   }
 
+  @ApiBody({
+    type: CreateGroupDto,
+  })
   @Post()
   createMyselfGroup(
     @Body() CreateGroupDto: CreateGroupDto & { userId: number },
@@ -37,16 +40,34 @@ export class GroupController {
     return this.groupService.findAll();
   }
 
+  @ApiParam({
+    name: 'id',
+    description: '그룹의 index ( ID )',
+    example: 1,
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.groupService.findOne(+id);
   }
 
+  @ApiBody({
+    type: UpdateGroupDto,
+  })
+  @ApiParam({
+    name: 'id',
+    description: '그룹의 index ( ID )',
+    example: 1,
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateGroupDto: UpdateGroupDto) {
     return this.groupService.update(+id, updateGroupDto);
   }
 
+  @ApiParam({
+    name: 'id',
+    description: '그룹의 index ( ID )',
+    example: 1,
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.groupService.remove(+id);
