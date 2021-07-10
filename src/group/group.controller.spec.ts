01823a42d5827cad16d919ba4794e1d58600ddcb
kakasoo <kscodebase@gmail.com>
2021-07-10 12:05:59 +0900

    test : group 생성 로직에 대한 테스트 작성 #1

--- src/group/group.controller.spec.ts
+++ src/group/group.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Groups } from './entities/group.entity';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
+
+class MockGroupRepository {}
 
 describe('GroupController', () => {
   let controller: GroupController;
@@ -8,7 +12,13 @@ describe('GroupController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GroupController],
-      providers: [GroupService],
+      providers: [
+        GroupService,
+        {
+          provide: getRepositoryToken(Groups),
+          useClass: MockGroupRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<GroupController>(GroupController);
--- src/group/group.service.spec.ts
+++ src/group/group.service.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateGroupDto } from './dto/create-group.dto';
+import { Groups } from './entities/group.entity';
 import { GroupService } from './group.service';
+
+import * as bcrypt from 'bcrypt';
+
+class MockGroupRepository {
+  save({ name, password, groupPic, visible }) {
+    return { name, password, groupPic, visible };
+  }
+}
 
 describe('GroupService', () => {
   let service: GroupService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GroupService],
+      providers: [
+        GroupService,
+        {
+          provide: getRepositoryToken(Groups),
+          useClass: MockGroupRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<GroupService>(GroupService);
   });
 
-  it('should be defined', () => {
+  it('그룹 서비스가 정의되어 있습니다.', () => {
     expect(service).toBeDefined();
   });
+
+  it('그룹이 추가되었습니다.', async () => {
+    const groupData: CreateGroupDto = {
+      name: 'kakasoo',
+      password: 'password',
+      groupPic: null,
+      visible: 1,
+    };
+
+    const { name, groupPic, visible, password } = await service.create(
+      groupData,
+    );
+
+    // 생성된 Group의 name, groupPic, visible, pasword가 잘 반영된 경우.
+    expect(name).toBe(groupData.name);
+    expect(groupPic).toBe(groupData.groupPic);
+    expect(visible).toBe(groupData.visible);
+    expect(bcrypt.compare(groupData.password, password)).resolves.toBe(true);
+  });
+
+  it.todo('그룹이 조회되었습니다.');
+  it.todo('그룹이 수정되었습니다.');
+  it.todo('그룹이 삭제되었습니다.');
 });
