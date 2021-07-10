bde43edd8e4c6b7af1fc7dfed95f1a182cfef4c6
kakasoo <kscodebase@gmail.com>
2021-07-10 18:15:51 +0900

    chore : swagger 문서에 controller 단의 request Params, body 설명 추가 #4

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,7 +10,7 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiParam, ApiTags } from '@nestjs/swagger';
 import { LoginUserDto } from './dto/login-user.dto';
 
 @ApiTags('USERS')
@@ -18,11 +18,17 @@ import { LoginUserDto } from './dto/login-user.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiBody({
+    type: CreateUserDto,
+  })
   @Post()
   signUp(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @ApiBody({
+    type: LoginUserDto,
+  })
   @Post()
   signIn(@Body() loginUserDto: LoginUserDto) {
     return this.userService.signIn(loginUserDto);
@@ -33,16 +39,34 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  @ApiParam({
+    name: 'id',
+    description: '유저의 index ( ID )',
+    example: 1,
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
+  @ApiBody({
+    type: UpdateUserDto,
+  })
+  @ApiParam({
+    name: 'id',
+    description: '유저의 index ( ID )',
+    example: 1,
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
+  @ApiParam({
+    name: 'id',
+    description: '유저의 index ( ID )',
+    example: 1,
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
