1622a388bfe278a60fb3f66d2ea8007cf5b3c314
kakasoo <kscodebase@gmail.com>
2021-07-10 18:52:38 +0900

    chore : entity에 apiProperties 추가 #5

--- src/diary/entities/diary.entity.ts
+++ src/diary/entities/diary.entity.ts
@@ -1,1 +1,80 @@
-export class Diary {}
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Users } from '../../user/entities/user.entity';
+import { Comments } from '../../comment/entities/comment.entity';
+import { Groups } from '../../group/entities/group.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Index('fk_diaries_users_idx', ['userId'], {})
+@Entity('DAIRIES', { schema: 'mydb' })
+export class Dairies {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'ID' })
+  id: number;
+
+  @Column('int', { name: 'USER_ID' })
+  userId: number;
+
+  @ApiProperty({
+    name: 'title',
+    description: '일기 제목',
+    example: '오늘 내가 한 일',
+  })
+  @Column('varchar', { name: 'TITLE', length: 45 })
+  title: string;
+
+  @ApiProperty({
+    name: 'content',
+    description: '일기 내용, 글자 수 제한 없음',
+    example: '오늘 나는 밥을 먹었다.',
+  })
+  @Column('longtext', { name: 'CONTENT', nullable: true })
+  content: string | null;
+
+  @ApiProperty({
+    name: 'hashtag',
+    description: '일기 해시 태그',
+    example: '#내가 #오늘 한 일 #밥',
+  })
+  @Column('longtext', { name: 'HASHTAG', nullable: true })
+  hashtag: string | null;
+
+  @Column('datetime', { name: 'CREATE_AT', default: () => 'CURRENT_TIMESTAMP' })
+  createAt: Date;
+
+  @Column('datetime', {
+    name: 'UPDATED_AT',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @Column('datetime', { name: 'DELETED_AT', nullable: true })
+  deletedAt: Date | null;
+
+  @ManyToOne(() => Users, (users) => users.dairies, {
+    onDelete: 'NO ACTION',
+    onUpdate: 'NO ACTION',
+  })
+  @JoinColumn([{ name: 'USER_ID', referencedColumnName: 'id' }])
+  user: Users;
+
+  @OneToMany(() => Comments, (comments) => comments.dairy)
+  comments: Comments[];
+
+  @ManyToMany(() => Groups, (groups) => groups.dairies)
+  @JoinTable({
+    name: 'DIARY_GROUPS',
+    joinColumns: [{ name: 'DAIRIES_ID', referencedColumnName: 'id' }],
+    inverseJoinColumns: [{ name: 'GROUPS_ID', referencedColumnName: 'id' }],
+    schema: 'mydb',
+  })
+  groups: Groups[];
+}
