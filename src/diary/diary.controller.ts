bf99b01f8cee4eb173b739c2ae0e0dcd4da205a0
kakasoo <kscodebase@gmail.com>
2021-07-10 19:43:54 +0900

    feat : controller 단에서 diary Request 프로퍼티 설명 추가 #5

--- src/diary/diary.controller.ts
+++ src/diary/diary.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { ApiParam, ApiProperty } from '@nestjs/swagger';
 import { DiaryService } from './diary.service';
 import { CreateDiaryDto } from './dto/create-diary.dto';
 import { UpdateDiaryDto } from './dto/update-diary.dto';
@@ -15,6 +16,9 @@ import { UpdateDiaryDto } from './dto/update-diary.dto';
 export class DiaryController {
   constructor(private readonly diaryService: DiaryService) {}
 
+  @ApiProperty({
+    type: CreateDiaryDto,
+  })
   @Post()
   create(@Body() createDiaryDto: CreateDiaryDto) {
     return this.diaryService.create(createDiaryDto);
@@ -25,16 +29,34 @@ export class DiaryController {
     return this.diaryService.findAll();
   }
 
+  @ApiParam({
+    name: 'id',
+    description: '일기의 index ( ID )',
+    example: 1,
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.diaryService.findOne(+id);
   }
 
+  @ApiProperty({
+    type: UpdateDiaryDto,
+  })
+  @ApiParam({
+    name: 'id',
+    description: '일기의 index ( ID )',
+    example: 1,
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateDiaryDto: UpdateDiaryDto) {
     return this.diaryService.update(+id, updateDiaryDto);
   }
 
+  @ApiParam({
+    name: 'id',
+    description: '일기의 index ( ID )',
+    example: 1,
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.diaryService.remove(+id);
