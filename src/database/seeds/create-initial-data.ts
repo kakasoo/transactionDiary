77a5fcb6437b13b2e058f9dbc030fd3733565fb9
kakasoo <kscodebase@gmail.com>
2021-07-11 12:04:56 +0900

    fix : seed로 넣는 user Data의 password를 hash #4

--- src/database/seeds/create-initial-data.ts
+++ src/database/seeds/create-initial-data.ts
@@ -2,6 +2,7 @@ import { Groups } from '../../group/entities/group.entity';
 import { Users } from '../../user/entities/user.entity';
 import { Connection } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
+import * as bcrypt from 'bcrypt';
 
 export class CreateInitialData implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
@@ -13,6 +14,7 @@ export class CreateInitialData implements Seeder {
       .execute();
 
     // NOTE : seeding으로 만든 data는 production에 절대 유입되지 않도록 한다.
+    const password = await bcrypt.hash('1234567890', 12);
     await connection
       .createQueryBuilder()
       .insert()
@@ -21,7 +23,7 @@ export class CreateInitialData implements Seeder {
         {
           id: 1,
           adress: 'kscodebase@gmail.com',
-          password: '1234567890',
+          password: password,
           nickname: 'kakasoo',
         },
       ])
