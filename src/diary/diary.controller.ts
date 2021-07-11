658d5f33ef3a4a3e182041424ae162c87400179e
kakasoo <kscodebase@gmail.com>
2021-07-11 13:40:53 +0900

    chore : swagger에 diary API summary 추가 #5

--- src/diary/diary.controller.ts
+++ src/diary/diary.controller.ts
@@ -7,7 +7,7 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
-import { ApiParam, ApiProperty, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiProperty, ApiTags } from '@nestjs/swagger';
 import { DiaryService } from './diary.service';
 import { CreateDiaryDto } from './dto/create-diary.dto';
 import { UpdateDiaryDto } from './dto/update-diary.dto';
@@ -17,6 +17,7 @@ import { UpdateDiaryDto } from './dto/update-diary.dto';
 export class DiaryController {
   constructor(private readonly diaryService: DiaryService) {}
 
+  @ApiOperation({ summary: '일기 작성' })
   @ApiProperty({
     type: CreateDiaryDto,
   })
@@ -25,11 +26,14 @@ export class DiaryController {
     return this.diaryService.create(createDiaryDto);
   }
 
+  // TODO : 각 유저가 조회할 수 있는 목록만을 가져오게 수정해야 한다.
+  @ApiOperation({ summary: '모든 일기 조회' })
   @Get()
   findAll() {
     return this.diaryService.findAll();
   }
 
+  @ApiOperation({ summary: '단일 일기 조회' })
   @ApiParam({
     name: 'id',
     description: '일기의 index ( ID )',
@@ -40,6 +44,7 @@ export class DiaryController {
     return this.diaryService.findOne(+id);
   }
 
+  @ApiOperation({ summary: '일기 내용 수정' })
   @ApiProperty({
     type: UpdateDiaryDto,
   })
@@ -53,6 +58,7 @@ export class DiaryController {
     return this.diaryService.update(+id, updateDiaryDto);
   }
 
+  @ApiOperation({ summary: '일기 내용 삭제' })
   @ApiParam({
     name: 'id',
     description: '일기의 index ( ID )',
