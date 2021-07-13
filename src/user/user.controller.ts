5e093a8f6c1a63fead530df87d2474508977ddd7
kakasoo <kscodebase@gmail.com>
2021-07-13 17:11:16 +0900

    style : controller의 생성 로직을 service로 이전 #4

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -37,32 +37,7 @@ export class UserController {
   })
   @Post('sign-up')
   async signUp(@Body() createUserDto: CreateUserDto) {
-    const { adress, password, nickname, userPic } = createUserDto;
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    const group = new Groups();
-    group.name = '내게쓰기';
-    group.password = hashedPassword;
-    group.readonly = 0;
-
-    const user = new Users();
-    user.adress = adress;
-    user.password = hashedPassword;
-    user.nickname = nickname;
-    user.userPic = userPic;
-    user.groups = [group];
-
-    const diary = new Dairies();
-    diary.title = '오신 것을 환영합니다!';
-    diary.content = '좋은 문화를 만들어요!';
-    diary.user = user;
-    diary.groups = [group];
-
-    await getManager().transaction(async (transactionEntityManager) => {
-      await transactionEntityManager.save(group);
-      await transactionEntityManager.save(user);
-      await transactionEntityManager.save(diary);
-    });
+    return this.userService.create(createUserDto);
   }
 
   @ApiOperation({ summary: '로그인' })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from '../user/entities/user.entity';
-import { Repository } from 'typeorm';
+import { getManager, Repository } from 'typeorm';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { UpdateUserDto } from '../user/dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
+import { Groups } from 'src/group/entities/group.entity';
+import { Dairies } from 'src/diary/entities/diary.entity';
 
 @Injectable()
 export class UserService {
@@ -16,12 +18,31 @@ export class UserService {
     const { adress, password, nickname, userPic } = createUserDto;
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    return this.userRepository.save({
-      adress,
-      password: hashedPassword,
-      nickname,
-      userPic,
+    const group = new Groups();
+    group.name = '내게쓰기';
+    group.password = hashedPassword;
+    group.readonly = 0;
+
+    const user = new Users();
+    user.adress = adress;
+    user.password = hashedPassword;
+    user.nickname = nickname;
+    user.userPic = userPic;
+    user.groups = [group];
+
+    const diary = new Dairies();
+    diary.title = '오신 것을 환영합니다!';
+    diary.content = '좋은 문화를 만들어요!';
+    diary.user = user;
+    diary.groups = [group];
+
+    await getManager().transaction(async (transactionEntityManager) => {
+      await transactionEntityManager.save(group);
+      await transactionEntityManager.save(user);
+      await transactionEntityManager.save(diary);
     });
+
+    return user;
   }
 
   async findAll() {
