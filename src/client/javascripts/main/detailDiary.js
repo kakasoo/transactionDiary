964ad4b307b2844a1ab756cef23af223c347bdaf
kakasoo <kscodebase@gmail.com>
2021-07-15 18:58:44 +0900

    fix : detailDiaryModal의 내용이 바뀌지 않는 문제 수정 #9

--- src/client/javascripts/main/detailDiary.js
+++ src/client/javascripts/main/detailDiary.js
@@ -5,21 +5,19 @@ class DetailDiary {
 
   getDetailDiaryModal(diaryId, groupId, title, content, updatedAt) {
     return function () {
+      console.log(diaryId, groupId, title, content);
       const diaryModal = document.getElementById('diaryModal');
-      const zIndex = diaryModal.style.zIndex;
+      // const zIndex = diaryModal.style.zIndex;
 
       // functions of util.js
       changeOpacity(diaryModal);
       changeZIndex(diaryModal);
 
-      // NOTE : modal이 켜지는 경우에 한하여 렌더링 시작.
-      if (zIndex === -10 || !zIndex) {
-        document.getElementById('diaryTitle').innerText = title;
-        document.getElementById('diaryContent').innerText = content;
-        document.getElementById('diaryDate').innerText = updatedAt;
+      document.getElementById('diaryTitle').innerText = title;
+      document.getElementById('diaryContent').innerText = content;
+      document.getElementById('diaryDate').innerText = updatedAt;
 
-        return;
-      }
+      return;
     };
   }
 }
