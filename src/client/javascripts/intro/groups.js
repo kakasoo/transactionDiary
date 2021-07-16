458e1cb263372d4a3e66797c83e090355db3a644
kakasoo <kscodebase@gmail.com>
2021-07-16 16:46:40 +0900

    feat : 그룹이 렌더링되도록 인트로 페이지 수정 #11

--- src/client/javascripts/intro/groups.js
+++ src/client/javascripts/intro/groups.js
@@ -1,11 +1,47 @@
 class GroupSection {
   constructor() {
     this.groupIntro = document.getElementById('groupIntro');
+    this.groupList = [];
+
     this.getGroups();
+  }
+
+  makeGroupLine(groups) {
+    const cardLine = document.createElement('section');
+    cardLine.style.display = 'flex';
+
+    for (const group of groups) {
+      const { id, name } = group;
+
+      const card = document.createElement('div');
+
+      card.id = `card${id}`;
+      card.className = 'card';
+      card.innerHTML = `
+      <div class = 'innerCard'>
+        <h2>${name}</h2>
+      </div>
+      `;
+
+      cardLine.appendChild(card);
+    }
+    return cardLine;
+  }
+
+  render() {
+    const GROUP_NUM = 5;
+    const groups = [...this.groupList];
+
+    while (groups.length) {
+      const groupLine = this.makeGroupLine(groups.splice(0, GROUP_NUM));
+      this.groupIntro.appendChild(groupLine);
+    }
   }
 
   async getGroups() {
     const groups = await getResourceByUrl('/api/groups/visible');
-    console.log(groups);
+    this.groupList.push(...groups);
+
+    this.render();
   }
 }
