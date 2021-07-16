5579563a0e75d9f100a90cd30f529d7a9e496ad0
kakasoo <kscodebase@gmail.com>
2021-07-16 21:48:39 +0900

    feat : 배너에 움직이는 텍스트 추가 #11

---
+++ src/client/javascripts/intro/banner.js
@@ -0,0 +1,40 @@
+class Banner {
+  constructor() {
+    this.banner = document.getElementById('bannerImg');
+    this.index = 0;
+    this.textContents = [
+      'Have a happy time.',
+      'Leave good memories.',
+      'Recall it occasionally.',
+    ];
+
+    this.setText(this.textContents[this.index++ % 3]);
+    setInterval(() => {
+      this.removeText();
+      this.setText(this.textContents[this.index++ % 3]);
+    }, 1500);
+  }
+
+  setText(textContent) {
+    const text = document.createElement('h1');
+    text.innerText = textContent;
+    text.style.height = '300px';
+    text.style.lineHeight = '300px';
+    text.style.width = '100%';
+    text.style.textAlign = 'center';
+    text.style.color = 'white';
+    text.style.fontSize = '108px';
+    text.style.textShadow = 'black 2px 2px 10px';
+
+    this.banner.style.display = 'flex';
+    this.banner.style.alignItems = 'center';
+
+    this.banner.appendChild(text);
+  }
+
+  removeText() {
+    while (this.banner.firstChild) {
+      this.banner.removeChild(this.banner.firstChild);
+    }
+  }
+}
--- src/client/javascripts/intro/index.js
+++ src/client/javascripts/intro/index.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+  new Banner();
   new LoginModal();
   new SignUpModal();
   new GroupSection();
