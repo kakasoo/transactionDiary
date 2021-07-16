8d9c9b8512bb48bd40f983b2cce27e6d2e1fa4cb
kakasoo <kscodebase@gmail.com>
2021-07-16 15:20:28 +0900

    feat : 회원가입 모달 #11

--- src/client/javascripts/intro/index.js
+++ src/client/javascripts/intro/index.js
@@ -1,3 +1,4 @@
 window.onload = () => {
   new LoginModal();
+  new SignUpModal();
 };
--- src/client/javascripts/intro/signUp.js
+++ src/client/javascripts/intro/signUp.js
@@ -1,3 +1,54 @@
 class SignUpModal {
-  constructor() {}
+  constructor() {
+    this.signUpButton = document.getElementById('signUpButton');
+    this.localSignUpButton = document.getElementById('localSignUp');
+
+    this.signUpButton.onclick = this.getSignUpModal;
+    this.localSignUpButton.onclick = this.localSignUp;
+  }
+
+  getSignUpModal() {
+    const signUpModal = document.getElementById('signUpModal');
+
+    // functions of util.js
+    changeOpacity(signUpModal);
+    changeZIndex(signUpModal);
+
+    const modalBackground = document.getElementById('modalBackground');
+    const modalBackgroundColor = modalBackground.style.background;
+    if (modalBackgroundColor === 'transparent' || !modalBackgroundColor) {
+      modalBackground.style.background = 'black';
+      modalBackground.style.zIndex = 5;
+    } else {
+      modalBackground.style.background = 'transparent';
+      modalBackground.style.zIndex = -10;
+    }
+  }
+
+  // TODO : password에서 enter 누를 시에 동작하게 해야 한다.
+  async localSignUp() {
+    const adress = document.getElementById('inputOfAdress').value;
+    const password = document.getElementById('inputOfPassword').value;
+    if (!adress || !password) {
+      // TODO : 경고 문구를 signUpModal에서 보여주면 좋을 것 같다.
+      // throw new Error('아이디나 비밀번호를 다시 확인해주세요!');
+      return;
+    }
+
+    const signUpResponse = await postDataByUrl(
+      '/api/users/signUp',
+      { adress, password },
+      false,
+    );
+
+    if (signUpResponse.ok) {
+      const { access_token } = await signUpResponse.json();
+      document.cookie = `auth=${access_token}`;
+      window.location.href = '/main';
+      return true;
+    }
+
+    alert('로그인에 실패하였습니다.');
+    return false;
+  }
 }
