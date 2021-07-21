197a886d17d3d2452413b9659dea30c661afd11f
kakasoo <kscodebase@gmail.com>
2021-07-21 19:29:14 +0900

    chore : typeorm을 사용하여 join query를 하는 방법을 나열 #14
    - service 코드의 NOTE 주석을 보면 알 수 있다.
    - innerJoin이 LeftOuterJoin보다 더 효율적이다.

--- src/diary/diary.module.ts
+++ src/diary/diary.module.ts
@@ -3,9 +3,11 @@ import { DiaryService } from './diary.service';
 import { DiaryController } from './diary.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Diaries } from './entities/diary.entity';
+import { UserGroups } from 'src/userGroup/entites/userGroup.entity';
+import { DiaryGroups } from 'src/diaryGroup/entites/diaryGroup.entity.ts';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Diaries])],
+  imports: [TypeOrmModule.forFeature([Diaries, UserGroups, DiaryGroups])],
   controllers: [DiaryController],
   providers: [DiaryService],
   exports: [DiaryService],
--- src/diary/diary.service.ts
+++ src/diary/diary.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { getConnection, Repository } from 'typeorm';
+import { DiaryGroups } from 'src/diaryGroup/entites/diaryGroup.entity.ts';
+import { UserGroups } from 'src/userGroup/entites/userGroup.entity';
+import { Connection, getConnection, Repository } from 'typeorm';
 import { CreateDiaryDto } from './dto/create-diary.dto';
 import { UpdateDiaryDto } from './dto/update-diary.dto';
 import { Diaries } from './entities/diary.entity';
@@ -8,39 +10,89 @@ import { Diaries } from './entities/diary.entity';
 @Injectable()
 export class DiaryService {
   constructor(
-    @InjectRepository(Diaries) private diaryRepository: Repository<Diaries>,
+    @InjectRepository(Diaries)
+    private diaryRepository: Repository<Diaries>,
+    @InjectRepository(UserGroups)
+    private userGroupRepository: Repository<UserGroups>,
+    @InjectRepository(DiaryGroups)
+    private diaryGroupRepository: Repository<DiaryGroups>,
+    private connection: Connection,
   ) {}
 
   async create(createDiaryDto: CreateDiaryDto, userId: number) {
-    // TODO : 자동으로 내게 쓰기 그룹에 가입되게 해야 한다.
-    // return diary;
-    const { groupIds } = createDiaryDto;
-    createDiaryDto.userId = userId;
+    console.log('here?');
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.startTransaction();
 
-    const diary = await this.diaryRepository.save(createDiaryDto);
-    const diaryId = diary.id;
+    try {
+      const { groupIds } = createDiaryDto;
+      delete createDiaryDto.groupIds;
+      createDiaryDto.userId = userId;
 
-    if (diary) {
-      const connection = getConnection();
+      // const diary = await this.diaryRepository.save(createDiaryDto);
 
-      // NOTE : 유저가 속한 내게 쓰기 그룹을 찾아서, groupIds에 추가한다.
-      const [myselfGroup] = await connection.manager.query(`
-        SELECT \`UG\`.\`GROUP_ID\` FROM \`USER_GROUPS\` AS \`UG\` LEFT OUTER JOIN \`GROUPS\` AS \`G\`
-        ON \`UG\`.\`GROUP_ID\` = \`G\`.\`ID\`
-        WHERE \`G\`.\`READONLY\` = 0 AND \`UG\`.\`USER_ID\` = ${userId}
-        LIMIT 1;
-        `);
+      // NOTE : createQueryBuilder 사용, columnName as otherName 형식이 가능하다.
+      const { groupId: myselfGroupId } = await this.userGroupRepository
+        .createQueryBuilder('userGroup')
+        .select(['GROUP_ID as groupId'])
+        .leftJoin('userGroup.group', 'group')
+        .where('group.READONLY = 1')
+        .andWhere(`userGroup.USER_ID = ${userId}`)
+        .getRawOne();
 
-      groupIds.push(myselfGroup.GROUP_ID);
+      // NOTE : find문과 option을 이용한 방식, join 또는 relations를 사용한다.
+      //      : 더 간단하게 사용가능하지만 as 문에 대응할만한 게 없다.
+      // const userGroup = await this.userGroupRepository.find({
+      //   // relations: ['group'],
+      //   join: {
+      //     alias: 'userGroups',
+      //     innerJoinAndSelect: {
+      //       group: 'userGroups.group',
+      //     },
+      //   },
+      //   select: ['userId', 'groupId', 'group'],
+      //   where: `group.readonly = 1 and userGroups.USER_ID = ${userId}`,
+      // });
 
-      for (const groupId of groupIds) {
-        await connection.manager.query(`
-          INSERT INTO DIARY_GROUPS(DIARY_ID, GROUP_ID) VALUES (${diaryId}, ${groupId});
-        `);
-      }
-
-      return diary;
+      console.log('userGroup : ', myselfGroupId);
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      console.error(error);
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
     }
+
+    // NOTE : 기존의 코드
+    // TODO : 자동으로 내게 쓰기 그룹에 가입되게 해야 한다.
+    // const { groupIds } = createDiaryDto;
+    // delete createDiaryDto.groupIds;
+    // createDiaryDto.userId = userId;
+
+    // const diary = await this.diaryRepository.save(createDiaryDto);
+    // const diaryId = diary.id;
+
+    // if (diary) {
+    //   const connection = getConnection();
+
+    //   // NOTE : 유저가 속한 내게 쓰기 그룹을 찾아서, groupIds에 추가한다.
+    //   const [myselfGroup] = await connection.manager.query(`
+    //     SELECT \`UG\`.\`GROUP_ID\` FROM \`USER_GROUPS\` AS \`UG\` LEFT OUTER JOIN \`GROUPS\` AS \`G\`
+    //     ON \`UG\`.\`GROUP_ID\` = \`G\`.\`ID\`
+    //     WHERE \`G\`.\`READONLY\` = 0 AND \`UG\`.\`USER_ID\` = ${userId}
+    //     LIMIT 1;
+    //     `);
+
+    //   groupIds.push(myselfGroup.GROUP_ID);
+
+    //   for (const groupId of groupIds) {
+    //     await connection.manager.query(`
+    //       INSERT INTO DIARY_GROUPS(DIARY_ID, GROUP_ID) VALUES (${diaryId}, ${groupId});
+    //     `);
+    //   }
+
+    //   return diary;
+    // }
   }
 
   async findAll(userId) {
