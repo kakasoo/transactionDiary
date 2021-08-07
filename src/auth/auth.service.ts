cba6b05510fc29a881657bc13141be9fb9ddbc8e
kakasoo <kscodebase@gmail.com>
2021-08-07 13:35:38 +0900

    fix : 토큰의 유효 시간을 1시간 이상으로 수정, 추후 리프레시 필요 #15

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -25,7 +25,9 @@ export class AuthService {
   async login(user: any) {
     const payload = { adress: user.adress, sub: user.id };
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign(payload, {
+        expiresIn: '1h',
+      }),
     };
   }
 }
